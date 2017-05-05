b0d2fd705c4ca37f4843732cc96f56c87333f2c4
azat-co <hi@azat.co>
2017-05-05 16:15:26 -0700

    add changes :incoming_envelope: :u6e80:

--- code/hello-world-server.js
+++ code/hello-world-server.js
@@ -6,12 +6,4 @@ require('http')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
-  })const port = 3000
-require('http')
-  .createServer((req, res) => {
-    console.log('url:', req.url)
-    res.end('hello world')
-  })
-  .listen(port, (error)=>{
-    console.log(`server is running on ${port}`)
   })
